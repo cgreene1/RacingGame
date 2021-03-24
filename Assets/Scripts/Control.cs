3655355f02a254b7f38c65fe6fa13743ea89f8c6
Gabby-Orr <gorr@trinity.edu>
2021-03-24 14:40:30 -0700

    Multiplayer Spawn and Movement
    - Made both players spawn into game after creating match
    - Players are able to move only their player
    - Set up code for assigning the players different spawn positions & colors
    - Still need winstate & game over stuff

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -1,35 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using System;
+using Photon.Realtime;
 
-public class Control : MonoBehaviour
+[RequireComponent(typeof(CharacterController))]
+
+public class Control : MonoBehaviourPun
 {
-    //Variables
-     public float speed = 20.0F; 
-     // Start is called before the first frame update
+    private CharacterController controller = null;
+
+    // ** don't need line below, bc it's a MonoBehaviourPun *** 
+    //  private PhotonView photonGuy = null;
+
+    [SerializeField] private float speed = 20.0F;
+
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+
+        SetUpBothPlayers();
     }
 
     // Update is called once per frame
-     void Update() {
-         Vector3 pos = transform.position;
+    void Update() {
+
+        // only move your player
+        if (photonView.IsMine)
+        {
+            TakeInput();
+        }
+
+        // **** below is initial code that wasn't photon-friendly ****
+
+         //Vector3 pos = transform.position;
  
-         if (Input.GetKey ("a")) {
-             pos.z += speed * Time.deltaTime;
-         }
-         if (Input.GetKey ("d")) {
-             pos.z -= speed * Time.deltaTime;
-         }
-         if (Input.GetKey ("w")) {
-             pos.x += speed * Time.deltaTime;
-         }
-         if (Input.GetKey ("s")) {
-             pos.x -= speed * Time.deltaTime;
-         }
+         //if (Input.GetKey ("a")) {
+         //    pos.z += speed * Time.deltaTime;
+         //}
+         //if (Input.GetKey ("d")) {
+         //    pos.z -= speed * Time.deltaTime;
+         //}
+         //if (Input.GetKey ("w")) {
+         //    pos.x += speed * Time.deltaTime;
+         //}
+         //if (Input.GetKey ("s")) {
+         //    pos.x -= speed * Time.deltaTime;
+         //}
              
  
-         transform.position = pos;
+         //transform.position = pos;
      }
+
+    private void SetUpBothPlayers()
+    {
+        int i = 0;
+        foreach (var player in PhotonNetwork.PlayerList)
+        {
+            if (i == 0)
+            {
+                // set material red
+                // make position on the left
+                Debug.Log($"Setting material/position for user: {i}");
+            }
+            else
+            {
+                // don't change material
+                // don't change position
+                Debug.Log($"Not changing material/position for user: {i}");
+            }
+
+            i++;
+        }
+    }
+
+    private void TakeInput()
+    {
+        Vector3 movement = new Vector3
+        {
+            x = Input.GetAxisRaw("Horizontal"),
+            y = 0f,
+            z = Input.GetAxisRaw("Vertical")
+        }.normalized;
+
+        controller.SimpleMove(movement * speed);
+    }
 }
