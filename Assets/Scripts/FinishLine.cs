2c30eb1f92afc9959fe1ba1b91ee2f2c1df0614b
Gabby-Orr <gorr@trinity.edu>
2021-03-26 12:58:58 -0700

    Added collider to player, got finishline to register player crossing line

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class FinishLine : MonoBehaviour
 {
-    private Boolean hasWon = false;
+    private bool hasWon = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +18,14 @@ public class FinishLine : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider player) {
-        if (PhotonNetwork.IsMasterClient) {
+    private void OnTriggerEnter(Collider col) {
+        //if (PhotonNetwork.IsMasterClient) {
+        if(col.gameObject.tag == "Car")
+        {
             hasWon = true;
             Debug.Log("Yay");
         }
+
     }
+
 }
